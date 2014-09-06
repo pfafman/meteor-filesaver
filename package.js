ed07fe8c1f698d525011c5e2cfefad5819bd400c
Timothy Pfafman <tim@pfafman.com>
2014-09-06 15:17:38 -0700

    update for meteor 0.9.  Sync with FileSaver.js

--- package.js
+++ package.js
@@ -1,7 +1,10 @@
 Package.describe({
-	summary: "filesaver.js packaged for use in Meteor."
+	summary: "filesaver.js packaged for use in Meteor.",
+  version: "0.1.0",
+  git: "https://github.com/pfafman/meteor-filesaver.git"
 });
 
 Package.on_use(function (api) {
+  api.versionsFrom("METEOR@0.9.0");
 	api.add_files('lib/FileSaver.js', 'client');
 });
