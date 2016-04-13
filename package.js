fb8a9ad614bd5b4572e309e8ddcfe2bca0416e61
Timothy Pfafman <tim@pfafman.com>
2016-04-13 13:12:19 -0700

    version bump to republish to atmosphere

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "pfafman:filesaver",
 	summary: "filesaver.js packaged for use in Meteor.",
-  version: "0.2.3",
+  version: "0.2.3_2",
   git: "https://github.com/pfafman/meteor-filesaver.git"
 });
 
