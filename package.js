61ea2ec49c4f0725475991ca2aaae3cf5f65d7c7
Timothy Pfafman <tim@pfafman.com>
2019-04-06 09:28:20 -0600

    update api

--- package.js
+++ package.js
@@ -1,12 +1,11 @@
 Package.describe({
   name: "pfafman:filesaver",
 	summary: "filesaver.js packaged for use in Meteor.",
-  version: "1.3.2",
+  version: "1.3.2_1",
   git: "https://github.com/pfafman/meteor-filesaver.git"
 });
 
 Package.onUse(function (api) {
   api.export(['saveAs'])
-  api.versionsFrom("METEOR@1.0");
-	api.add_files('lib/FileSaver.js', 'client');
+  api.add_files('lib/FileSaver.js', 'client');
 });
